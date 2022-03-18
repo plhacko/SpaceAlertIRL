fe830d446b74a3718c46a1746c046794b3ac8431
Plhák Tomáš <plhacko@gmail.com>
2022-03-18 14:35:44 +0100

    bug fix -> Enemy Icon Panel now updates itself

--- SpaceAlertIRL/Assets/Scripts/Ship/Enemies/Enemy.cs
+++ SpaceAlertIRL/Assets/Scripts/Ship/Enemies/Enemy.cs
@@ -22,15 +22,15 @@ public abstract class Enemy : NetworkBehaviour
 
     protected virtual void Start()
     {
-        Zone = GetComponentInParent<Zone>();
-        Zone.AddEnemy(this);
-
-        // TODO: this used to be problem elsewere (If I remember correctly) -> this needs to be set up only on server
         _HP = new NetworkVariable<int>(StratingHPConst);
         _EnergyShields = new NetworkVariable<int>(StartingEnergyShieldsConst);
 
         UIActions.AddOnValueChangeDependency(_HP);
         UIActions.AddOnValueChangeDependency(_EnergyShields);
+
+        Zone = GetComponentInParent<Zone>();
+        Zone.AddEnemy(this);
+        Zone.UIActions.UpdateUI();
     }
 
     abstract public void TakeDamage(int damage, Weapon w); // the weapon is needed if there is a special exception
