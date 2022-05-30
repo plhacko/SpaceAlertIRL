08fcbc6e03d7119fc8b683ba346e4b6bc5406731
Plhák Tomáš <plhacko@gmail.com>
2022-05-30 16:03:52 +0200

    server updater
    now works only if it is in RoomScene
    so the simulation doesn't start with the host

--- SpaceAlertIRL/Assets/Scripts/Testing/ServerUpdater.cs
+++ SpaceAlertIRL/Assets/Scripts/Testing/ServerUpdater.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.SceneManagement;
 
 interface IOnServerFixedUpdate
 {
@@ -13,6 +14,9 @@ public class ServerUpdater : NetworkBehaviour
     void FixedUpdate()
     {
         if (!NetworkManager.Singleton.IsServer) { return; } //TODO: rm
+
+        if (SceneManager.GetActiveScene().name != "RoomScene") { return; } // TODO: rethink and make it a optimal solution
+
         var arrayToUpdate = GetComponentsInChildren<IOnServerFixedUpdate>();
         foreach (var i in arrayToUpdate)
         {
