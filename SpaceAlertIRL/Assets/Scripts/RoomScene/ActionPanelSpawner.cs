4e30392a1094c0b58ed6e55149c08428332ca936
plhacko <73901161+plhacko@users.noreply.github.com>
2023-02-26 01:49:52 +0100

    better action panel

--- SpaceAlertIRL/Assets/Scripts/RoomScene/ActionPanelSpawner.cs
+++ SpaceAlertIRL/Assets/Scripts/RoomScene/ActionPanelSpawner.cs
@@ -1,19 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ActionPanelSpawner : MonoBehaviour
 {
+    Color Color;
+
     public void ResetSelf()
     {
+        GetComponent<Image>().color = Color;
+
         foreach (Transform child in transform)
         {
             GameObject.Destroy(child.gameObject);
         }
     }
 
+    public void HideActionPanel()
+    {
+        ResetSelf();
+        GetComponent<Image>().color = new Color(0, 0, 0, 0);
+    }
+
     private void Start()
     {
+        Color = GetComponent<Image>().color;
+
         ResetSelf();
 
         if (!Player.GetLocalPlayer().IsConnectedToPanel.Value)
